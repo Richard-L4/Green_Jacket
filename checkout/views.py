c6fc235e4d094dd419e9a9177906a5552f91492a
Richard-L4 <rwells945@gmail.com>
2025-07-01 19:38:39 +0100

    work on payments page

--- checkout/views.py
+++ checkout/views.py
@@ -1,37 +1,118 @@
-import json
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
+from django.conf import settings
+
 from .forms import OrderForm
+from .models import Order, OrderLineItem
+from items.models import Item
+from trolley.contexts import trolley_contents
+
+import stripe
 
 
 def checkout(request):
-    trolley = request.session.get('trolley', {})
-    
-    if not trolley:
-        messages.error(request, "There's nothing in your trolley at the moment")
-        return redirect(reverse('items'))
-    
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     if request.method == 'POST':
-        order_form = OrderForm(request.POST)
+        bag = request.session.get('trolley', {})
+
+        form_data = {
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+            'country': request.POST['country'],
+            'postcode': request.POST['postcode'],
+            'town_or_city': request.POST['town_or_city'],
+            'street_address1': request.POST['street_address1'],
+            'street_address2': request.POST['street_address2'],
+            'county': request.POST['county'],
+        }
+        order_form = OrderForm(form_data)
         if order_form.is_valid():
-            order = order_form.save(commit=False)
-            # Save trolley data as JSON string in order
-            order.original_trolley = json.dumps(trolley)
-            order.save()
-            # Here you would also create order line items etc.
-            
-            # Clear trolley after order is saved
-            request.session['trolley'] = {}
-            messages.success(request, 'Order successfully placed!')
-            return redirect(reverse('order_success', args=[order.order_number]))
+            order = order_form.save()
+            for item_id, item_data in bag.items():
+                try:
+                    item = Item.objects.get(id=item_id)
+                    if isinstance(item_data, int):
+                        order_line_item = OrderLineItem(
+                            order=order,
+                            item=item,
+                            quantity=item_data,
+                        )
+                        order_line_item.save()
+                    else:
+                        for size, quantity in item_data['items_by_size'].items():
+                            order_line_item = OrderLineItem(
+                                order=order,
+                                item=item,
+                                quantity=quantity,
+                                item_size=size,
+                            )
+                            order_line_item.save()
+                except Item.DoesNotExist:
+                    messages.error(request, (
+                        "One of the items in your bag wasn't found in our database. "
+                        "Please call us for assistance!")
+                    )
+                    order.delete()
+                    return redirect(reverse('view_trolley'))
+
+            request.session['save_info'] = 'save-info' in request.POST
+            return redirect(reverse('checkout_success', args=[order.order_number]))
         else:
-            messages.error(request, 'There was an error with your form. Please check and try again.')
+            messages.error(request, 'There was an error with your form. \
+                Please double check your information.')
     else:
+        trolley = request.session.get('trolley', {})
+        if not trolley:
+            messages.error(request, "There's nothing in your bag at the moment")
+            return redirect(reverse('items'))
+
+        current_trolley = trolley_contents(request)
+        total = current_trolley['grand_total']
+        stripe_total = round(total * 100)
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
+
         order_form = OrderForm()
-    
-    template = 'checkout/checkout.html'
+
+        # in the video, the below code is not indented properly
+        # this is the correct indentation
+        if not stripe_public_key:
+            messages.warning(request, 'Stripe public key is missing. \
+                Did you forget to set it in your environment?')
+
+        template = 'checkout/checkout.html'
+        context = {
+            'order_form': order_form,
+            'stripe_public_key': stripe_public_key,
+            'client_secret': intent.client_secret,
+        }
+
+        return render(request, template, context)
+        # end of the corrected indentation
+
+
+def checkout_success(request, order_number):
+    """
+    Handle successful checkouts
+    """
+    save_info = request.session.get('save_info')
+    order = get_object_or_404(Order, order_number=order_number)
+    messages.success(request, f'Order successfully processed! \
+        Your order number is {order_number}. A confirmation \
+        email will be sent to {order.email}.')
+
+    if 'trolley' in request.session:
+        del request.session['trolley']
+
+    template = 'checkout/checkout_success.html'
     context = {
-        'order_form': order_form,
+        'order': order,
     }
 
     return render(request, template, context)
